72e0f4cc0c702df875c46e0a936eeb41686cae7b
ntan03 <91350990+ntan03@users.noreply.github.com>
2024-07-12 04:39:01 -0400

    created canvas states/typings for toolbar and future canvas edits

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/app/types/canvas";
+import { useHistory, useCanUndo, useCanRedo } from "@liveblocks/react/suspense";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -9,11 +13,34 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
+  const undo = () => {
+    history.undo();
+  };
+
+  const redo = () => {
+    history.redo();
+  };
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        undo={undo}
+        redo={redo}
+        canUndo={canUndo}
+        canRedo={canRedo}
+      />
     </main>
   );
 };
