613e38650f47105b17e5eca25a98f5ad90188ea5
ntan03 <91350990+ntan03@users.noreply.github.com>
2024-07-13 12:30:38 -0400

    finished selection tools, allowing bring to front/send to back

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -10,7 +10,7 @@ import { Camera, Color } from "@/types/canvas";
 import { ColorPicker } from "./color-picker";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -21,6 +21,7 @@ export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
 
+    // Change fill color hook
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
         const liveLayers = storage.get("layers");
@@ -33,6 +34,47 @@ export const SelectionTools = memo(
       [selection, setLastUsedColor]
     );
 
+    // Bring to front / send to back
+    const bringToFront = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get("layerIds");
+        const arr = liveLayerIds.toArray();
+
+        const indices: Array<number> = [];
+        for (let i = 0; i < arr.length; i++) {
+          if (selection?.includes(arr[i])) indices.push(i);
+        }
+
+        // Positioning is dependent on their position in the array
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayerIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
+        }
+      },
+      [selection]
+    );
+
+    const sendToBack = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get("layerIds");
+        const arr = liveLayerIds.toArray();
+
+        const indices: Array<number> = [];
+        for (let i = 0; i < arr.length; i++) {
+          if (selection?.includes(arr[i])) indices.push(i);
+        }
+
+        // Positioning is dependent on their position in the array
+        for (let i = 0; i < indices.length; i++) {
+          liveLayerIds.move(indices[i], i);
+        }
+      },
+      [selection]
+    );
+
+    // Delete layer hook
     const deleteLayers = useDeleteLayers();
 
     // Moved to bottom due to issues with hook count between renders (unselecting crashes)
@@ -53,6 +95,18 @@ export const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label="Bring to front">
+            <Button variant="board" size="icon" onClick={bringToFront}>
+              <BringToFront />
+            </Button>
+          </Hint>
+          <Hint label="Send to back" side="bottom">
+            <Button variant="board" size="icon" onClick={sendToBack}>
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label="Delete">
             <Button variant="board" size="icon" onClick={deleteLayers}>
